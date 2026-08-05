c612181eb123ed57f247e065c950e98a45ec04dd
ausdisau <australiandisabilitycontact@gmail.com>
2026-08-05 21:36:44 +1000

    feat(moodle): add DMS theme metadata

---
+++ moodle/theme/dms/version.php
@@ -0,0 +1,15 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'theme_dms';
+$plugin->version = 2026080500;
+$plugin->requires = 2024042200; // Moodle 4.4 or later.
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '0.1.0';
